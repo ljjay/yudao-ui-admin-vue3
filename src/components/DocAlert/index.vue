62662984091ce9ae1d4189839a825a7262ad61a5
ljj <249191716@qq.com>
2025-02-24 17:24:51 +0800

    取消doc-alert控件

--- src/components/DocAlert/index.vue
+++ src/components/DocAlert/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <el-alert v-if="getEnable()" type="success" show-icon>
+  <template></template>
+<!--  <el-alert v-if="getEnable()" type="success" show-icon>
     <template #title>
       <div @click="goToUrl">{{ '【' + title + '】文档地址：' + url }}</div>
     </template>
-  </el-alert>
+  </el-alert>-->
 </template>
 <script setup lang="tsx">
 import { propTypes } from '@/utils/propTypes'
